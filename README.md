447a512fdebb4473e397a8db43b1e069ce2a6a08
Andreas Weber <code@andreas-weber.me>
2015-01-19 13:04:11 +0100

    Intial commit.

---
+++ README.md
@@ -0,0 +1,16 @@
+#Puppet Module - Packages
+This module manages the available default packages.
+
+##Dependencies
+
+None.
+
+## Puppetfile
+
+```
+mod 'aw_packages', :git => 'https://github.com/andreas-weber/puppet-packages.git'
+```
+
+## Usage
+
+See puppet manifests.
---
+++ manifests/init.pp
@@ -0,0 +1,33 @@
+# == Class: aw_packages
+#
+# This module manages the available default packages.
+#
+# === Parameters
+#
+# === Examples
+#
+#   include aw_packages
+#
+#   class { 'aw_packages': }
+#
+# === Authors
+#
+# Andreas Weber <code@andreas-weber.me>
+#
+# === Copyright
+#
+# Copyright 2015 Andreas Weber
+#
+class aw_packages()
+{
+  package {
+    [
+      'wget',
+      'htop',
+      'curl',
+      'ant',
+      'git'
+    ]:
+      ensure => 'latest'
+  }
+}
